c5354c1c0a308aa9689a2bc338c062bedb9a2b0c
Dave Randall <dave@goldsquare.co.uk>
2018-10-26 17:49:12 +0100

    Upgrade on daily basis

--- terraform/cloudwatch-rules.tf
+++ terraform/cloudwatch-rules.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudwatch_event_rule" "pis-ssm-daily" {
   name                = "pis-ssm-daily"
   description         = "Run the pis-ssm-daily playbook on all pis"
-  schedule_expression = "rate(10 minutes)"
+  schedule_expression = "rate(1 days)"
 }
 
 resource "aws_cloudwatch_event_target" "pis-ssm-daily" {
